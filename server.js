f719625574e74eb22d34a9a78f379e1463fc1d9a
Jimmya2007 <jimjesus1986@gmail.com>
2026-07-24 00:00:03 -0400

    Initialize anime-sdk server with providers

---
+++ server.js
@@ -0,0 +1,18 @@
+import { startServer, MegaPlayProvider, AnikotoProvider, HttpClient } from 'anime-sdk';
+
+const store = new Map();
+
+startServer({
+  providers: [
+    new MegaPlayProvider(new HttpClient({ timeoutMs: 15000 })),
+    new AnikotoProvider(new HttpClient({ timeoutMs: 15000 })),
+  ],
+  port: process.env.PORT || 3000,
+  proxy: true,
+  cache: {
+    get: (k) => store.get(k),
+    set: (k, v) => store.set(k, v),
+  },
+});
+
+console.log('anime-sdk server running!');
